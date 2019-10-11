ca5ed37d4d9e51ff384c8ef2507b1f88d4769573
mcnew <ix552-github@yahoo.com>
2019-10-11 15:50:53 +0000

    Add services & database

---
+++ db/ddl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `color` (
+	  `id` int(11) NOT NULL AUTO_INCREMENT,
+	  `name` varchar(50),
+	  `description` varchar(100),
+	  `date` date,
+	  `startTime` time,
+	  `endTime` time,
+	  PRIMARY KEY (`id`)
+);
+

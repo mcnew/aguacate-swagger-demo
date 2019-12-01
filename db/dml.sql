6f14084c878316708ba0d35e9448bb3066220e48
mcnew <ix552-github@yahoo.com>
2019-12-01 00:19:13 -0500

    Update dml.sql

--- db/dml.sql
+++ db/dml.sql
@@ -1,2 +1,2 @@
-INSERT INTO perfect(code, value) VALUES('89ed1', 'Lorem ipsum dolor sit amet');
-INSERT INTO perfect(code, value) VALUES('29ef50, 'Curabitur pretium tincidunt lacus');
+INSERT INTO perfect(code, value) VALUES('89ed1', 'Lorem ipsum dolor');
+INSERT INTO perfect(code, value) VALUES('29ef50, 'Curabitur pretium');

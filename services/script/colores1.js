0bab33733bee4f727926c03dc8259a8487033934
mcnew <ix552-github@yahoo.com>
2020-01-27 23:17:21 -0500

    [functionality] sync changes

--- services/script/colores1.js
+++ services/script/colores1.js
@@ -11,6 +11,18 @@
 					message : 'The description does not contain the name'
 				};
 			}
+		},
+		updateCheckValidation : function(description, date, starTime, endTime, active) {
+			var isModification = description !== null || date !== null || starTime !== null || endTime !== null;
+			if (isModification && active !== null) {
+				return {
+					message : 'Don\'t modify active and any other attibute'
+				};
+			} else {
+				return {
+					value : 'success'
+				};
+			}
 		}
 	}
 })();
